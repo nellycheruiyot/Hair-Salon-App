de2ab3d314bbb32b3d392911f2f890095c65fed6
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-12-15 16:44:28 +0300

    Update Spark interface to utilize RESTful routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -24,20 +24,33 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("clients", request.session().attribute("clients"));
+      // model.put("clients", request.session().attribute("clients"));
       // model.put("stylists", Stylist.all());
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("clients/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/client-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/clients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("clients", Client.all());
+      model.put("template", "templates/clients.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     post("/clients", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
-      ArrayList<Client> clients = request.session().attribute("clients");
-      if (clients == null) {
-        clients = new ArrayList<Client>();
-        request.session().attribute("clients", clients);
-      }
+      // ArrayList<Client> clients = request.session().attribute("clients");
+      // if (clients == null) {
+      //   clients = new ArrayList<Client>();
+      //   request.session().attribute("clients", clients);
+      // }
       // Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
       String name = request.queryParams("name");
       Client newClient = new Client(name);
@@ -49,6 +62,14 @@ public class App {
       // model.put("stylist", stylist);
       model.put("template", "templates/success.vtl");
       // model.put("template", "templates/stylist-clients-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/clients/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Client client = Client.find(Integer.parseInt(request.params(":id")));
+      model.put("client", client);
+      model.put("template", "templates/client.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
