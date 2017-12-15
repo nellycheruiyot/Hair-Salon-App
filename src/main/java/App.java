5431b9a140a6d5327cd99b2a15d2895a855a49c6
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-12-15 12:01:31 +0300

    Modify to create a root route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,5 +22,13 @@ public class App {
 
     setPort(port);
 
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      // model.put("tasks", request.session().attribute("tasks"));
+      // model.put("stylists", Stylist.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
