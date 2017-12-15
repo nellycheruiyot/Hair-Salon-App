5aff955728c04479f61eb49af3b82a498a57168e
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-12-15 11:19:51 +0300

    Add set up basics

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
+
+  }
+}
